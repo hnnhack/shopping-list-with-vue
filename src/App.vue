d271af592cd8c8e8a7b75d2f1d85a3beab855194
hnnhack <hojaniyazhan@yahoo.com>
2024-03-23 09:41:41 +0100

    v-if and v-else

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,11 @@
 import { ref } from 'vue'
 
 const header = ref('Shopping List App')
+const editing = ref(false)
 const items = ref([
-  {id: 1, label: "10 party hats"},
-  {id: 2, label: "2 board games"},
-  {id: 3, label: "20 cups"}
+  // {id: 1, label: "10 party hats"},
+  // {id: 2, label: "2 board games"},
+  // {id: 3, label: "20 cups"}
 ])
 const newItem = ref('')
 const newItemHighPriority = ref(false)
@@ -13,12 +14,21 @@ const saveItem = () => {
   items.value.push({id: items.value.length + 1, label: newItem.value})
   newItem.value = ''
 }
+const doEdit = (e) => {
+  editing.value = e
+  newItem.value = ''
+}
 </script>
 
 <template>
-  <h1>{{ header }}</h1>
+  <div class="header">
+    <h1>{{ header }}</h1>
+    <button v-if="editing" class="btn" @click="doEdit(false)">Cancel</button>
+    <button v-else class="btn btn-primary" @click="doEdit(true)">Add Item</button>
+  </div>
   <form 
     @submit.prevent="saveItem"
+    v-if="editing"
     class="add-item-form"
   >
     <input v-model.trim="newItem" type="text" placeholder="Add an item">
@@ -31,13 +41,16 @@ const saveItem = () => {
     </label>
     <button
       class="btn btn-primary"
-    >Add Item</button>
+    >Save Item</button>
   </form>
   <ul>
     <li v-for="{id, label} in items" :key="id">
       {{label}}
     </li>
   </ul>
+  <p v-if='!items.length'>
+    List is empty
+  </p>
 </template>
 
 <style src="./main.css" lang="css"></style>
