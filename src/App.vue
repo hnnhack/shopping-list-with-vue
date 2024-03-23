3be6298cf31ee2e15307bd911a2dfe8aaf9b907b
hnnhack <hojaniyazhan@yahoo.com>
2024-03-23 07:46:41 +0100

    initialize state with radio button and check box

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,23 @@ const items = ref([
   {id: 2, label: "2 board games"},
   {id: 3, label: "20 cups"}
 ])
+const newItem = ref()
+const newItemHighPriority = ref(false)
 </script>
 
 <template>
   <h1>{{ header }}</h1>
+  <input v-model="newItem" type="text" placeholder="Add an item">
+  {{ newItemHighPriority ? 'High' : 'Low' }}
+  <br />
+  <label>
+    <input type="checkbox" v-model="newItemHighPriority"> 
+    High Priority
+  </label>
+  <select v-model="newItemHighPriority">
+    <option value="false">Low</option>
+    <option value="true">High</option>
+  </select>
   <ul>
     <li v-for="({id, label}, index) in items" :key="id">
       {{ index + 1 + '-' }}
