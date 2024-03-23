768c3fa00a1e12a1808dd09a6ca936d4ac8b800e
hnnhack <hojaniyazhan@yahoo.com>
2024-03-23 08:55:04 +0100

    on click and enter key to add new item to the list

--- src/App.vue
+++ src/App.vue
@@ -7,36 +7,29 @@ const items = ref([
   {id: 2, label: "2 board games"},
   {id: 3, label: "20 cups"}
 ])
-const newItem = ref()
+const newItem = ref('')
 const newItemHighPriority = ref(false)
-const iceCreamFlavors = ref([])
 </script>
 
 <template>
   <h1>{{ header }}</h1>
-  <input v-model="newItem" type="text" placeholder="Add an item">
-  {{ iceCreamFlavors }}
-  <br />
-  <label>
-    <input type="checkbox" v-model="newItemHighPriority"> 
-    High Priority
-  </label>
-  <br />
-  <label>
-    <input type="checkbox" v-model="iceCreamFlavors" value="Vanilla"> 
-    Vanilla
-  </label>
-  <label>
-    <input type="checkbox" v-model="iceCreamFlavors" value="Strawberry"> 
-    Strawberry
-  </label>
-  <label>
-    <input type="checkbox" v-model="iceCreamFlavors" value="Chocolate"> 
-    Chocolate
-  </label>
+  <form class="add-item-form">
+    <input v-model="newItem" type="text" placeholder="Add an item">
+    <label>
+      <input 
+        type="checkbox" 
+        v-model="newItemHighPriority"
+        v-on:keyup.enter="items.push({id: items.length + 1, label: newItem})"
+      > 
+      High Priority
+    </label>
+    <button
+      v-on:click="items.push({id: items.length + 1, label: newItem})"
+      class="btn btn-primary"
+    >Add Item</button>
+  </form>
   <ul>
-    <li v-for="({id, label}, index) in items" :key="id">
-      {{ index + 1 + '-' }}
+    <li v-for="{id, label} in items" :key="id">
       {{label}}
     </li>
   </ul>
